5ee61c93278080d96d7559a29ee2e3b4a25db230
Chao Chen <chao98052@gmail.com>
2018-03-08 12:00:32 -0800

    Add lidar camera auto detection

--- lidar.py
+++ lidar.py
@@ -1,4 +1,4 @@
-# Copyright [2018] [MicroVision]
+# Copyright 2018 MicroVision, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,23 +11,37 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 # Written by Chao Chen
 import cv2 as cv
 import numpy as np
 
 # Capture from camera at location 1. The number could change depending on how many cameras are connected to this computer.
-cam = cv.VideoCapture(0)
-# our depth sensing camera output format has 120*720 pixels. Each pixel has 4 byes, 2 for depth, 2 for amplitude.
-# Since python OpenCV only support BRG3 format, our lidar will report 160*720 BGR3 format with 3 bytes/pixel. 
-# cam.set(cv.cv.CV_CAP_PROP_FOURCC, cv.cv.CV_FOURCC('B', 'G', 'R', '3'))		#This line is needed for OpenCV2.0
-cam.set(3, 160)    # set the width cv.cv.CV_CAP_PROP_FRAME_WIDTH 160
-cam.set(4, 720)    # set the height cv.cv.CV_CAP_PROP_FRAME_HEIGHT 720
+foundLidar = False
+for i in range (0, 3) :
+    cam = cv.VideoCapture(i)
+    if cam :
+        width = cam.get(3)
+        height = cam.get(4)
+        if (width == 120 or width == 160) and height == 720 :
+            # cam.set(cv.cv.CV_CAP_PROP_FOURCC, cv.cv.CV_FOURCC('B', 'G', 'R', '3'))		#This line is needed for OpenCV2.*
+            cam.set(3, 160)    # set the width cv.cv.CV_CAP_PROP_FRAME_WIDTH 160
+            cam.set(4, 720)    # set the height cv.cv.CV_CAP_PROP_FRAME_HEIGHT 720
+            foundLidar = True
+            break;
+        cam.release()
+
+if foundLidar :
+    print "Start Lidar camera. Hit Esc key to quit"
+else :
+    print "Couldn't detect MicroVision Lidar camera. Exit..."
 
 # grab camera frames and display
-print "Hit Esc key to quit"
-while True:
+while foundLidar:
     ret, img = cam.read()
-    # Reshaped img to 720*120*4 format
+    # Our depth sensing camera output format has 120*720 pixels. Each pixel has 4 byes, 2 for depth, 2 for amplitude.
+    # Since python OpenCV only support BRG3 format, our lidar will report 160*720 BGR3 format with 3 bytes/pixel. 
+    # Reshaped img from 720*160*3 to 720*120*4 format
     img = np.reshape(img, [720, 120, 4])
     # img[:, :, 0:1] are depth data. Squeeze to 8 bits for gray scale display. The real depth data could be out of displayed range.
     depth0 = img[:, :, 0]
@@ -42,7 +56,7 @@ while True:
     # stack depth image on top of amplitude image
     combined = np.vstack((depthImg, ampImg))
     rimg = cv.resize(combined, (960, 720), interpolation = cv.INTER_CUBIC)
-    cv.imshow("Depth Camera", rimg)
+    cv.imshow("Lidar Camera", rimg)
     key = cv.waitKey(10)
     if key == 27:   # Esc key
         break
